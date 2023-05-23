64885a8e40c3b9677dfa6c1289d71f4a49a338e5
BoJoNVi <bonjomontes@gmail.com>
2023-05-23 10:52:00 +0800

    build: added robots.txt generation to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import { astroImageTools } from "astro-imagetools";
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), astroImageTools, sitemap()]
+  integrations: [tailwind(), mdx(), astroImageTools, sitemap(), robotsTxt()]
 });
