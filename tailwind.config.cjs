258dc2202dbfe1837c66f0285ec90b7aa8902582
BoJoNVi <bonjomontes@gmail.com>
2023-05-23 11:51:00 +0800

    feat: implemented simple layout for navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bg1: "#FBF7F4",
+        c1: "#334B80",
+      },
+    },
+  },
+  plugins: [],
+};
