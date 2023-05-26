477b9d677a14b1cded2f0f8bf9d986dea6c6ea31
BoJoNVi <bonjomontes@gmail.com>
2023-05-26 17:08:57 +0800

    feat: implemented getting latest currency to PHP

---
+++ scripts/updateCurrency.ts
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+export default async function updateCurrency() {
+  const request = await fetch("https://api.exchangerate-api.com/v4/latest/PHP");
+
+  const data = await request.json();
+
+  fs.writeFileSync(
+    "./data/currency-rates.json",
+    JSON.stringify(data.rates, null, 2),
+    "utf-8"
+  );
+}
+
+updateCurrency();
