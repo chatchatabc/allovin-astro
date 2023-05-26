371b784628d5fc0fc5b3fbe6d5227b21993de3ee
BoJoNVi <bonjomontes@gmail.com>
2023-05-26 17:10:33 +0800

    feat: implemented rate conversion

---
+++ src/domain/services/utilService.ts
@@ -0,0 +1,12 @@
+import currencyRatesJson from "@data/currency-rates.json";
+
+export function utilConvertToPhp(amount: number, currency: string) {
+  const currencyRates = currencyRatesJson as Record<string, number>;
+  const rate = currencyRates[currency];
+
+  if (!rate) {
+    return undefined;
+  }
+
+  return amount * rate;
+}
