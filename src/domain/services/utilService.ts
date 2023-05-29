76e345d60781ff02449ff24715337d8be852219d
BoJoNVi <bonjomontes@gmail.com>
2023-05-29 11:04:04 +0800

    feat: implemented color variant optimizer

--- src/domain/services/utilService.ts
+++ src/domain/services/utilService.ts
@@ -1,4 +1,6 @@
 import currencyRatesJson from "@data/currency-rates.json";
+import colorsJson from "@data/colors.json";
+import type { CommonVariants } from "../models/commonModel";
 
 export function utilConvertToPhp(amount: number, currency: string) {
   const currencyRates = currencyRatesJson as Record<string, number>;
@@ -9,4 +11,27 @@ export function utilConvertToPhp(amount: number, currency: string) {
   }
 
   return amount / rate;
+}
+
+export function utilOptimizeColorVariants(list: CommonVariants[]) {
+  const colors = colorsJson.contents;
+
+  const activeColors: string[] = [];
+  const newList: CommonVariants[] = [];
+
+  list.forEach((variant) => {
+    const variantColor = colors.find((color) => {
+      return variant.title.toLowerCase().includes(color.toLocaleLowerCase());
+    });
+
+    if (variantColor && !activeColors.includes(variantColor)) {
+      newList.push({
+        title: variantColor,
+        image: variant.image,
+      });
+      activeColors.push(variantColor);
+    }
+  });
+
+  return newList;
 }
