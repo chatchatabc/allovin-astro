1839101ee530d89de3097593c95bcef795b3b469
BoJoNVi <bonjomontes@gmail.com>
2023-05-30 16:43:50 +0800

    feat: added tags on the products scraping

--- scripts/getProducts.ts
+++ scripts/getProducts.ts
@@ -22,7 +22,7 @@ export default async function getProducts() {
 
     const productData = request.data.product;
 
-    console.log(productData, index * 1000, product);
+    console.log(index * 1000, product);
 
     if (productData) {
       product = { ...product, ...productData };
@@ -31,12 +31,14 @@ export default async function getProducts() {
     completeData.push(product);
 
     if (index === data.length - 1) {
-      setTimeout(() => {
-        fs.writeFileSync(
-          "./data/product-items.json",
-          JSON.stringify(completeData, null, 2)
-        );
-      }, index * 1000 + 10000);
+      await new Promise((resolve) => setTimeout(resolve, index * 1000 + 10000));
+
+      console.log("Saving file...");
+      fs.writeFileSync(
+        "./data/product-items.json",
+        JSON.stringify(completeData, null, 2)
+      );
+      console.log("File saved!");
     }
   });
 }
